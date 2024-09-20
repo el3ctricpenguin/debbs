c798e70cccd9ad603e62d8728853102f557267cc
el3ctricpenguin <el3ctricpenguin@protonmail.com>
2024-09-20 22:34:40 +0800

    frontend: add wagmi

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,11 +1,20 @@
 import theme from "@/config/theme";
+import { wagmiConfig } from "@/config/wagmi";
 import { ChakraProvider } from "@chakra-ui/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import type { AppProps } from "next/app";
+import { WagmiProvider } from "wagmi";
+
+const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
         <ChakraProvider theme={theme}>
-            <Component {...pageProps} />
+            <WagmiProvider config={wagmiConfig}>
+                <QueryClientProvider client={queryClient}>
+                    <Component {...pageProps} />
+                </QueryClientProvider>
+            </WagmiProvider>
         </ChakraProvider>
     );
 }
