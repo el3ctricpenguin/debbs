29afb71a3d75d268556391c26ecfa7634767235f
el3ctricpenguin <el3ctricpenguin@protonmail.com>
2024-09-21 03:13:31 +0800

    frontend: add TUI table for threads

--- frontend/src/pages/board/[boardId].tsx
+++ frontend/src/pages/board/[boardId].tsx
@@ -1,37 +1,57 @@
 import { BBSHeading } from "@/components/BBSHeading";
 import BBSLayout from "@/components/BBSLayout";
-import { chakra, Link, Text } from "@chakra-ui/react";
+import { Link, Text } from "@chakra-ui/react";
 import Head from "next/head";
 import NextLink from "next/link";
+import Table from "cli-table3";
 
 export default function Home() {
-    const boardsResult = [
-        { boardTitle: "Ethereum", boardId: 0 },
-        { boardTitle: "DeFi", boardId: 0 },
-        { boardTitle: "Trading", boardId: 0 },
-        { boardTitle: "NFT", boardId: 0 },
-        { boardTitle: "Prediction Market", boardId: 0 },
-        { boardTitle: "GameFi", boardId: 0 },
-        { boardTitle: "Wallet Security", boardId: 0 },
-    ];
+    const boardResult = { boardTitle: "Ethereum", boardId: 0 };
+    const createThreadFee = 0.0001;
 
-    const recentThreadsResult = [
+    const threadsResult = [
         {
             threadId: 0,
             account: "great-security.eth",
             threadDescription: "I made a special anti-fraud wallet, Pizza Wallet.",
+            postCount: 120,
         },
         {
             threadId: 0,
             account: "0xd3ef...ad823",
             threadDescription: "Where can I get WBTC at better price?",
+            postCount: 21,
         },
         {
             threadId: 0,
             account: "house-boy.eth",
             threadDescription: "I’m crypto bilionaire living with my mom",
+            postCount: 6,
         },
     ];
+
+    const table = new Table({
+        head: ["Account", "Title", "Earned Fees"],
+        colWidths: [20, 60, 15],
+        chars: {
+            top: "",
+            "top-mid": "",
+            "top-left": "",
+            "top-right": "",
+            bottom: "",
+            "bottom-mid": "",
+            "bottom-left": "",
+            "bottom-right": "",
+        },
+    });
+    table.push(
+        ...threadsResult.map(({ account, threadDescription, postCount }) => [
+            account,
+            threadDescription,
+            (Math.floor(createThreadFee * postCount * 10000) / 10000).toFixed(4).toString() + " ETH",
+        ])
+    );
+    console.log(table.toString());
 
     const recentPostsResult = [
         {
@@ -51,7 +71,6 @@ export default function Home() {
         },
     ];
 
-    const Hr = chakra("hr");
     return (
         <>
             <Head>
@@ -61,31 +80,10 @@ export default function Home() {
             </Head>
             <BBSLayout>
                 <>
-                    <BBSHeading headingProps={{ mb: 2 }}>&gt; Boards</BBSHeading>
-                    <Text>
-                        {boardsResult.map((board, i) => (
-                            <>
-                                {i == 0 ? "" : " / "}
-                                <Link as={NextLink} href={`/board/${board.boardId}`} key={i}>
-                                    {board.boardTitle}
-                                </Link>
-                            </>
-                        ))}
+                    <BBSHeading headingProps={{ mt: 4, mb: 2 }}>&gt; Threads</BBSHeading>
+                    <Text whiteSpace="pre-wrap" fontFamily="monospace" fontSize={11}>
+                        {table.toString()}
                     </Text>
-                    <Text>[See More]</Text>
-                    <Hr borderStyle="dashed" my={2} />
-
-                    <BBSHeading headingProps={{ mt: 4, mb: 2 }}>&gt; Recent Threads</BBSHeading>
-                    {recentThreadsResult.map((thread, i) => (
-                        <Text key={i}>
-                            <Link as={NextLink} href={`/account/${thread.account}`}>
-                                [{thread.account}]
-                            </Link>{" "}
-                            <Link as={NextLink} href={`/thread/${thread.threadId}`}>
-                                {thread.threadDescription}
-                            </Link>
-                        </Text>
-                    ))}
 
                     <BBSHeading headingProps={{ mt: 4, mb: 2 }}>&gt; Recent Posts</BBSHeading>
                     {recentPostsResult.map((post, i) => (
