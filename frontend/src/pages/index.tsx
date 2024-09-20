3467aaa6a6e453f95492cf9faa4a0254806359bc
el3ctricpenguin <el3ctricpenguin@protonmail.com>
2024-09-20 22:35:00 +0800

    frontend: add connect/disconnect button

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,7 +1,12 @@
-import { Button } from "@chakra-ui/react";
+import { Button, Text } from "@chakra-ui/react";
 import Head from "next/head";
+import { useAccount, useConnect, useDisconnect } from "wagmi";
 
 export default function Home() {
+    const { address } = useAccount();
+    const { connect, connectors } = useConnect();
+    const { disconnect } = useDisconnect();
+
     return (
         <>
             <Head>
@@ -9,7 +14,9 @@ export default function Home() {
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <Button>Chakra Test</Button>
+            <Button onClick={() => connect({ connector: connectors[0] })}>Connect</Button>
+            <Button onClick={() => disconnect()}>Disconnect</Button>
+            <Text>{address}</Text>
         </>
     );
 }
