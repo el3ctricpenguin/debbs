2306e3b26b1a32bff16082bb95ce959ec373710d
el3ctricpenguin <el3ctricpenguin@protonmail.com>
2024-09-21 16:38:38 +0800

    frontend: add Linea Sepolia network

--- frontend/src/config/wagmi.ts
+++ frontend/src/config/wagmi.ts
@@ -1,13 +1,14 @@
 import { http, createConfig } from "wagmi";
-import { mainnet, sepolia } from "wagmi/chains";
+import { lineaSepolia, mainnet, sepolia } from "wagmi/chains";
 import { injected } from "wagmi/connectors";
 
 export const wagmiConfig = createConfig({
-    chains: [mainnet, sepolia],
+    chains: [mainnet, sepolia, lineaSepolia],
     connectors: [injected()],
     transports: {
         [mainnet.id]: http(),
         [sepolia.id]: http("https://ethereum-sepolia-rpc.publicnode.com"),
+        [lineaSepolia.id]: http("https://ethereum-sepolia-rpc.publicnode.com"),
     },
     ssr: true,
 });
