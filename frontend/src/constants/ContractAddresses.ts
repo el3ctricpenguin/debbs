f65a9c09648a2d56ad5d2f2dc736f7c51a1e1df2
el3ctricpenguin <el3ctricpenguin@protonmail.com>
2024-09-22 01:41:19 +0800

    frontend: update ca

--- frontend/src/constants/ContractAddresses.ts
+++ frontend/src/constants/ContractAddresses.ts
@@ -4,7 +4,7 @@ import { lineaSepolia, sepolia } from "viem/chains";
 export function getDeBBSAddress(chainId?: number) {
     switch (chainId) {
         case sepolia.id:
-            return getAddress("0x0B479E42C1EEdeAFb38f49085c4ceaE318db9748");
+            return getAddress("0xCc97a26eEc318Ccc23163dC982fb948313603C68");
         case lineaSepolia.id:
             return getAddress("0x86fAE1E990d1D5E96a199F6f797Ae7F144b420E9");
         default:
