627782a111081f0a195227429979a5df798f6080
el3ctricpenguin <el3ctricpenguin@protonmail.com>
2024-09-21 18:57:10 +0800

    frontend: add tooltip for Post component

--- frontend/src/components/Post.tsx
+++ frontend/src/components/Post.tsx
@@ -1,5 +1,5 @@
 import { convertTimestampToLocalTime } from "@/utils/convertTimestampToLocalTime";
-import { Box, VStack, HStack, Text, chakra } from "@chakra-ui/react";
+import { Box, VStack, HStack, Text, chakra, Tooltip } from "@chakra-ui/react";
 import { ColorsContext } from "@/config/ColorContext";
 import { useContext } from "react";
 import { EnsNameOrAddress } from "@/components/EnsNameOrAddress";
@@ -19,7 +19,7 @@ type PostProps = {
 export default function Post({ post }: { post: PostProps }) {
     const colors = useContext(ColorsContext);
     const primaryColor = colors[0];
-    // const bgColor = colors[1];
+    const bgColor = colors[1];
 
     const Hr = chakra("hr");
     const svgString = jdenticon.toSvg(post.postOwner, 20);
@@ -30,9 +30,26 @@ export default function Post({ post }: { post: PostProps }) {
                     <Box border={`1px solid ${primaryColor}`}>
                         <Box w={5} h={5} dangerouslySetInnerHTML={{ __html: svgString }}></Box>
                     </Box>
-                    <Text>
-                        <EnsNameOrAddress address={post.postOwner} shorten />
-                    </Text>
+                    <Tooltip
+                        label={
+                            <Box>
+                                <Text fontWeight={900}>
+                                    &gt; <EnsNameOrAddress address={post.postOwner} shorten />
+                                </Text>
+                            </Box>
+                            // TODO: create a component here
+                        }
+                        placement="bottom-start"
+                        bgColor={bgColor}
+                        boxShadow="none"
+                        border={`1px solid ${primaryColor}`}
+                        borderRadius={0}
+                        offset={[0, 4]}
+                    >
+                        <Text>
+                            <EnsNameOrAddress address={post.postOwner} shorten />
+                        </Text>
+                    </Tooltip>
                 </HStack>
                 <Text>{post.postContent}</Text>
                 <Text>[{convertTimestampToLocalTime(Number(post.timestamp))}]</Text>
