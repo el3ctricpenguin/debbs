503df43fa65be69450bdcab54652a7ccdecb3e30
el3ctricpenguin <el3ctricpenguin@protonmail.com>
2024-09-21 22:02:37 +0800

    frontend: The Graph Integration + add Recent Threads/Posts at Dashboard

---
+++ frontend/src/utils/theGraphFetcher.ts
@@ -0,0 +1,12 @@
+export const theGraphFetcher = async (query: string) => {
+    const response = await fetch("https://api.studio.thegraph.com/query/88257/debbs_linea_sepolia/version/latest", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ query }),
+    });
+
+    const data = await response.json();
+    return data;
+};
