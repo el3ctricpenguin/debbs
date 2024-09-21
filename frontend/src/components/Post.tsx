d7444f18179d185025c5f5437ee75fcaee06191d
el3ctricpenguin <el3ctricpenguin@protonmail.com>
2024-09-21 19:16:12 +0800

    frontend: add link to Post user

--- frontend/src/components/Post.tsx
+++ frontend/src/components/Post.tsx
@@ -1,10 +1,11 @@
 import { convertTimestampToLocalTime } from "@/utils/convertTimestampToLocalTime";
-import { Box, VStack, HStack, Text, chakra, Tooltip } from "@chakra-ui/react";
+import { Box, VStack, HStack, Text, chakra, Tooltip, Link } from "@chakra-ui/react";
 import { ColorsContext } from "@/config/ColorContext";
 import { useContext } from "react";
 import { EnsNameOrAddress } from "@/components/EnsNameOrAddress";
 import * as jdenticon from "jdenticon";
 import { Address } from "viem";
+import NextLink from "next/link";
 
 type PostProps = {
     postId: bigint;
@@ -27,9 +28,11 @@ export default function Post({ post }: { post: PostProps }) {
         <Box>
             <VStack spacing={2} align="start">
                 <HStack justify="left">
-                    <Box border={`1px solid ${primaryColor}`}>
-                        <Box w={5} h={5} dangerouslySetInnerHTML={{ __html: svgString }}></Box>
-                    </Box>
+                    <Link as={NextLink} href={`/user/${post.postOwner}`}>
+                        <Box border={`1px solid ${primaryColor}`}>
+                            <Box w={5} h={5} dangerouslySetInnerHTML={{ __html: svgString }}></Box>
+                        </Box>
+                    </Link>
                     <Tooltip
                         label={
                             <Box>
@@ -46,9 +49,11 @@ export default function Post({ post }: { post: PostProps }) {
                         borderRadius={0}
                         offset={[0, 4]}
                     >
-                        <Text>
-                            <EnsNameOrAddress address={post.postOwner} shorten />
-                        </Text>
+                        <Link as={NextLink} href={`/user/${post.postOwner}`}>
+                            <Text>
+                                <EnsNameOrAddress address={post.postOwner} shorten />
+                            </Text>
+                        </Link>
                     </Tooltip>
                 </HStack>
                 <Text>{post.postContent}</Text>
