366ecf0c3341e3291b3b1a538322c39ff4056480
el3ctricpenguin <el3ctricpenguin@protonmail.com>
2024-09-21 21:02:48 +0800

    frontend: update contract address

--- frontend/src/constants/ContractAddresses.ts
+++ frontend/src/constants/ContractAddresses.ts
@@ -6,7 +6,7 @@ export function getDeBBSAddress(chainId?: number) {
         case sepolia.id:
             return getAddress("0x0B479E42C1EEdeAFb38f49085c4ceaE318db9748");
         case lineaSepolia.id:
-            return getAddress("0xb3CAEee35bC9330D253BE2901e2eB51091b7D1AB");
+            return getAddress("0x86fAE1E990d1D5E96a199F6f797Ae7F144b420E9");
         default:
             return zeroAddress;
     }
